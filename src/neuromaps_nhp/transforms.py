5f08bf6f46b116f3a61de5bda60c0c79fc3f8848
Tamsin Rogers <tamsinrog@gmail.com>
2025-10-02 17:08:45 -0400

    ruff

--- src/neuromaps_nhp/transforms.py
+++ src/neuromaps_nhp/transforms.py
@@ -1,23 +1,25 @@
-from pathlib import Path
-from niwrap import ants     #add to utils
-import nibabel as nib
+"""Functions for volumetric transformations niwrap."""
 
-'''Extract voxel spacing from a NIfTI file using wb_command.'''
-def _extract_res(nii_file: Path):
+from pathlib import Path
+
+import nibabel as nib
+from niwrap import ants  #add to utils
+
+
+def _extract_res(nii_file: Path) -> tuple[float, float, float]:
     """Extract voxel spacing from a NIfTI file using nibabel."""
     img = nib.load(nii_file)
     return img.header.get_zooms()[:3]  
 
-'''Transform a volumetric image from source space to target space.'''
-def _vol_to_vol(source: Path, target: Path):
-
+def _vol_to_vol(source: Path, target: Path) -> Path:
+    """Transform a volumetric image from source space to target space."""
     # later - include in the networkx graph, fetch and assert that the path exists
 
     '''
-    Can add a warning in the future if we want to allow for volumes to be resampled back 
-    to the original resolution in the target space,but generally, we would expect the output 
-    to be in the resolution of the target image. Given we are transforming to the target space, 
-    the warning is left out for now.
+    Can add a warning in the future if we want to allow for volumes to be resampled 
+    back to the original resolution in the target space,but generally, we would expect 
+    the output to be in the resolution of the target image. Given we are transforming 
+    to the target space, the warning is left out for now.
     '''
 
     out_file = target.parent / f"{source.stem}_to_{target.stem}.nii.gz"
