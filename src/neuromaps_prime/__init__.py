a52699719bbd14f9bdc3950ef34556ca3919e31f
Biraj <birajstha@gmail.com>
2025-10-10 21:27:43 +0000

    adding changes suggested by Jason

--- src/neuromaps_prime/__init__.py
+++ src/neuromaps_prime/__init__.py
@@ -3,8 +3,3 @@
 This package provides tools for transforming and analyzing neuroimaging data
 in non-human primates.
 """
-
-from .config import Config
-from .utils import gifti_utils, niwrap_wrappers
-
-config = Config()
