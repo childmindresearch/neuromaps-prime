61e4631921d68acbefc3c26901526fb3cd69e00c
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-11-11 09:59:09 -0500

    Reset runner after testing setting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,13 @@
 from typing import Any
 
 import pytest
-from niwrap import DockerRunner, LocalRunner, SingularityRunner, get_global_runner
+from niwrap import (
+    DockerRunner,
+    LocalRunner,
+    SingularityRunner,
+    get_global_runner,
+    set_global_runner,
+)
 
 from neuromaps_prime import utils
 
@@ -12,7 +18,7 @@ class TestSetRunner:
     """Testing of set runner utility method."""
 
     @pytest.mark.parametrize(
-        "runner, expected_runner",
+        "test_runner, expected_runner",
         [
             ("local", LocalRunner),
             ("docker", DockerRunner),
@@ -21,17 +27,21 @@ class TestSetRunner:
     )
     def test_set_runner(
         self,
-        runner: str,
+        test_runner: str,
         expected_runner: LocalRunner | DockerRunner | SingularityRunner,
     ):
         """Test setting runner."""
-        images = {"container1": "/path/to/container"} if runner == "singularity" else {}
-        utils.set_runner(runner=runner, image_map=images)
+        runner = get_global_runner()
+        images = (
+            {"container1": "/path/to/container"} if test_runner == "singularity" else {}
+        )
+        utils.set_runner(runner=test_runner, image_map=images)
         test_runner = get_global_runner()
         assert isinstance(test_runner, expected_runner)
+        set_global_runner(runner)
 
     @pytest.mark.parametrize(
-        "runner, expected_runner",
+        "test_runner, expected_runner",
         [
             ("LOCAL", LocalRunner),
             ("LoCaL", LocalRunner),
@@ -43,21 +53,23 @@ class TestSetRunner:
     )
     def test_runner_case_insensitive(
         self,
-        runner: str,
+        test_runner: str,
         expected_runner: LocalRunner | DockerRunner | SingularityRunner,
     ):
         """Test case-insensitivty of setting runner."""
+        runner = get_global_runner()
         images = (
             {"container1": "/path/to/container"}
-            if runner.lower() == "singularity"
+            if test_runner.lower() == "singularity"
             else {}
         )
-        utils.set_runner(runner=runner, image_map=images)
+        utils.set_runner(runner=test_runner, image_map=images)
         test_runner = get_global_runner()
         assert isinstance(test_runner, expected_runner)
+        set_global_runner(runner)
 
     @pytest.mark.parametrize(
-        "runner, expected_runner",
+        "input_runner, expected_runner",
         [
             ("local", LocalRunner),
             ("docker", DockerRunner),
@@ -66,39 +78,55 @@ class TestSetRunner:
     )
     def test_runner_valid_kwargs(
         self,
-        runner: str,
+        input_runner: str,
         expected_runner: LocalRunner | DockerRunner | SingularityRunner,
     ):
         """Test passing valid kwargs to set local runner."""
+        runner = get_global_runner()
         data_dir = "/path/to/data/dir"
-        images = {"container1": "/path/to/container"} if runner == "singularity" else {}
-        utils.set_runner(runner=runner, image_map=images, data_dir=data_dir)
+        images = (
+            {"container1": "/path/to/container"}
+            if input_runner == "singularity"
+            else {}
+        )
+        utils.set_runner(runner=input_runner, image_map=images, data_dir=data_dir)
         test_runner = get_global_runner()
         assert isinstance(test_runner, expected_runner)
         assert str(test_runner.data_dir) == data_dir
+        set_global_runner(runner)
 
-    @pytest.mark.parametrize("runner", ("local", "docker", "singularity"))
-    def test_runner_invalid_kwargs(self, runner: str):
+    @pytest.mark.parametrize("test_runner", ("local", "docker", "singularity"))
+    def test_runner_invalid_kwargs(self, test_runner: str):
         """Test passing invalid kwargs to set local runner."""
-        images = {"container1": "/path/to/container"} if runner == "singularity" else {}
+        runner = get_global_runner()
+        images = (
+            {"container1": "/path/to/container"} if test_runner == "singularity" else {}
+        )
         with pytest.raises(TypeError, match="unexpected keyword argument"):
-            utils.set_runner(runner=runner, image_map=images, arg="val123")
+            utils.set_runner(runner=test_runner, image_map=images, arg="val123")
+        set_global_runner(runner)
 
     # empty map
     def test_empty_image_map(self):
         """Test exception raised when no container mappins for singularity runner."""
+        runner = get_global_runner()
         with pytest.raises(ValueError, match="No container mappings"):
             utils.set_runner("singularity", image_map={})
+        set_global_runner(runner)
 
     # invalid map
     @pytest.mark.parametrize("image_map", (set(), [], ()))
     def test_invalid_image_map_type(self, image_map: Any):
         """Test exception raised when map is not a dict."""
+        runner = get_global_runner()
         with pytest.raises(TypeError, match="Expected image_map dictionary"):
             utils.set_runner(runner="local", image_map=image_map)
+        set_global_runner(runner)
 
     # not implemented type
     def test_unimplemented_runner(self):
         """Test exception raised for unimplemented runners."""
+        runner = get_global_runner()
         with pytest.raises(NotImplementedError, match="not implemented"):
             utils.set_runner(runner="invalid")
+        set_global_runner(runner)
