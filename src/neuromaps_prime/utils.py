b3de97ec9ee219aa88ffb331c34725fe1baa1e32
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-11-12 13:00:31 -0500

    Update StyxSingularity (#39)
    * Update dependencies
    
    * StyxSingularity update
    
    New version of StyxSingularity will use the singularity cache for automatically
    downloading and using containers. This commit makes the relevant changes to
    accomodate for this.

--- src/neuromaps_prime/utils.py
+++ src/neuromaps_prime/utils.py
@@ -5,36 +5,37 @@ from niwrap import use_docker, use_local, use_singularity
 
 def set_runner(
     runner: str,
-    image_map: dict[str, str] | None = None,
+    image_overrides: dict[str, str] | None = None,
     **kwargs,
 ) -> None:
     """Set StyxRunner to use for NiWrap.
 
     Args:
         runner: Styx runner type to use (one of 'local', 'docker', 'singularity).
-        image_map: Optional dictionary of container paths to override.
+        image_overrides: Optional dictionary of container tag overrides.
 
     Raises:
-        TypeError: if image_map is not dictionary.
-        ValueError: if singularity runner is used and image_map not provided.
+        TypeError: if image_overrides is not dictionary.
         NotImplementedError: if provided runner not a valid StyxRunner.
     """
-    if image_map is not None and not isinstance(image_map, dict):
-        raise TypeError(f"Expected image_map dictionary, got {type(image_map)}")
+    if image_overrides is not None and not isinstance(image_overrides, dict):
+        raise TypeError(
+            f"Expected image_overrides dictionary, got {type(image_overrides)}"
+        )
     match runner_exec := runner.lower():
         case "local":
             use_local(**kwargs)
         case "docker":
             use_docker(
                 docker_executable=runner_exec,
-                image_overrides=image_map,
+                image_overrides=image_overrides,
                 **kwargs,
             )
         case "singularity":
-            if not image_map:
-                raise ValueError("No container mappings provided.")
             use_singularity(
-                singularity_executable=runner_exec, images=image_map, **kwargs
+                singularity_executable=runner_exec,
+                image_overrides=image_overrides,
+                **kwargs,
             )
         case _:
             raise NotImplementedError(f"'{runner_exec}' runner not implemented.")
