b91eb04434b2e71e11d3021e48bb07ff719e28d8
Jason Kai <21226986+kaitj@users.noreply.github.com>
2026-04-29 16:19:25 -0400

    Update init from multi files

--- src/neuromaps_prime/resources/__init__.py
+++ src/neuromaps_prime/resources/__init__.py
@@ -14,16 +14,26 @@ _ROOT = Path(__file__).parent.resolve()
 
 
 class NeuromapsPrimeYAML(NamedTuple):
-    """Path to default Neuromaps-PRIME graph config.
+    """Path to default Neuromaps-PRIME graph resources.
 
     Attributes:
         yaml: Default graph framework for neuromaps-prime.
     """
 
-    yaml: Path
+    nodes: tuple[Path, ...]
+    surface_edges: tuple[Path, ...]
+    volume_edges: tuple[Path, ...]
 
 
-NEUROMAPSPRIME_GRAPH = NeuromapsPrimeYAML(yaml=_ROOT / "neuromaps_graph.yaml")
+def _rglob(subdir: str) -> tuple[Path, ...]:
+    return tuple(sorted((_ROOT / subdir).rglob("*.yaml")))
+
+
+NEUROMAPSPRIME_GRAPH = NeuromapsPrimeYAML(
+    nodes=_rglob("nodes"),
+    surface_edges=_rglob("edges/surface"),
+    volume_edges=_rglob("edges/volume"),
+)
 
 
 __all__ = ["NEUROMAPSPRIME_GRAPH"]
