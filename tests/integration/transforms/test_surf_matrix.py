990a042c9544c467b2a5597d11e5e133af322f4c
Tamsin Rogers <tamsinrog@gmail.com>
2026-05-14 22:38:32 -0400

    comment

--- tests/integration/transforms/test_surf_matrix.py
+++ tests/integration/transforms/test_surf_matrix.py
@@ -130,7 +130,7 @@ def test_surface_transform_matrix(tmp_path: Path) -> None:
             "new-area": dst_surface,
         }
 
-        # resample because the surfaces are not in the same space
+        # resample because the surfaces are not the same mesh
         workbench.surface_resample(
             surface_in=str(src_surface),
             current_sphere=str(src_sphere),
