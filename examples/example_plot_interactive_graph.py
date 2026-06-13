88de073d977040ecb89e709696f102f2db448e23
Tamsin Rogers <tamsinrog@gmail.com>
2026-06-13 15:19:18 +0200

    transform

--- examples/example_plot_interactive_graph.py
+++ examples/example_plot_interactive_graph.py
@@ -11,7 +11,7 @@ color_lut = {
     'macaque': "#DEAC52",   # Amber
     'human': "#95B46A",  # Sage green
     'chimpanzee': "#C8D0D5",   # Slate gray
-    'marmoset': "#C8D0D5",   # Red
+    'marmoset': "#e98c8cff",   # Red
     'volume_to_volume': '#E08E9A', # Pink
     'surface_to_surface': '#80B2D4', # Blue
     'both': '#A28DC7' # Purple
